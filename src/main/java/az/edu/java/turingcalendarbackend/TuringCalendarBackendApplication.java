71819d747ddb99cc9dbdc1921ad736487b69352b
HP ProBook <kalbaliyevaselen@gmai.com>
2025-03-30 18:13:18 +0400

    first commit

---
+++ src/main/java/az/edu/java/turingcalendarbackend/TuringCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package az.edu.java.turingcalendarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringCalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringCalendarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/edu/java/turingcalendarbackend/TuringCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.java.turingcalendarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringCalendarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
